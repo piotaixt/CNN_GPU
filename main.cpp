71f074e721b755f6dda0709deda826b63a1d2e95
Thomas Piotaix <thomas@piotaix.fr>
2022-01-04 00:30:22 +0100

    Début de convolution bis

--- main.cpp
+++ main.cpp
@@ -13,30 +13,12 @@ using namespace cv;
 #define SIZE_TEST 100
 #define SIZE_TRAIN 1000
 
-// void fill_the_data(float *test_set_3, float *test_set_4, float *train_set_3, float *train_set_4) {
-
-//   for (int i = 0; i < SIZE_TEST; i++) {
-//     string str_type_3(to_string(3));
-//     string str_number(to_string(i));
-
-//     Mat image = imread("test_set/" + str_type + "/" + str_number + ".jpg", 0);
-//     if (image.empty()) {
-//       std::cout << "image non trouvée" << std::endl;
-//     }
-
-//     cout << image << endl;
-
-//     fill imshow("imge chargée", image);
-//     waitKey(0);
-//   }
-// }
-
 float sigmoid_C1(float a) {
   // A modifier.
   return a;
 }
 
-Mat convolution_C1(Mat &image, float coefficients[5][5][6], int size, int num_layer) {
+Mat convolution_C1(Mat &image, float coefficients[6][5][5], int size, int num_layer, float bias) {
   int cumul = 0;
   Mat result = Mat::zeros(cv::Size(size - 4, size - 4), CV_8U);
   for (int i = 2; i < size - 2; i++) {
@@ -55,32 +37,33 @@ Mat convolution_C1(Mat &image, float coefficients[5][5][6], int size, int num_la
 
 int main() {
 
-  // uint test_set_3[32][32][SIZE_TEST];
-  // uint test_set_4[32][32][SIZE_TEST];
-  // uint train_set_3[32][32][SIZE_TRAIN];
-  // uint train_set_4[32][32][SIZE_TRAIN];
-
-  // Les paramètres que l'on va modifier, sur lesquels on va faire du training.
-  float coefficients_first_convolution[5][5][6];
+  // On charge une image pour tester
 
   string str_type_3(to_string(3));
   string str_number(to_string(5));
-
   Mat image = imread("test_set/" + str_type_3 + "/" + str_number + ".jpg", 0);
   if (image.empty()) {
     std::cout << "image non trouvée" << std::endl;
   }
-
   cout << image << endl << endl;
 
-  Mat image_convol = convolution_C1(image, coefficients_first_convolution, 32, 1);
+  // Les paramètres que l'on va modifier, sur lesquels on va faire du training.
+  float coefficients_C1[6][5][5];
+  float bias_C1[6];
 
+  // initialisation des paramètres
+  for (int i = 0; i < 6; i++) {
+    bias_C1[i] = 1;
+    for (int j = 0; j < 5; j++) {
+      for (int k = 0; k < 5; k++) {
+        coefficients_C1[i][j][k] = 1;
+      }
+    }
+  }
+
+  // On calcule un layer du C1
+  Mat image_convol = convolution_C1(image, coefficients_C1, 32, 1, bias_C1[1]);
   cout << image_convol << endl;
-
-  imshow("imge chargée", image);
-  waitKey(0);
-
-  // fill_the_data(test_set_3, test_set_4, train_set_3, train_set_4);
 
   return 0;
 }
